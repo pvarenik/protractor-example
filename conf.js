9ecf9943d24d651a2a8512a5823ba310a075b90c
pvarenik <pavel.varenik@pleeco.com>
2016-12-20 18:22:50 +0300

    minor

--- conf.js
+++ conf.js
@@ -7,12 +7,12 @@ exports.config = {
      */
     //seleniumServerJar: "node_modules/protractor/node_modules/webdriver-manager/selenium/selenium-server-standalone-2.53.3.jar",
     directConnect: true,
-    
+
     specs: ['specs/*Spec.js'],
     baseUrl: 'http://qualityshepherd.com',
     framework: 'jasmine2',
 
-    onPrepare: function(){
+    onPrepare: function () {
         // set implicit wait times in ms...
         browser.manage().timeouts().implicitlyWait(5000);
         // set browser size...
@@ -20,19 +20,19 @@ exports.config = {
 
         // better jasmine 2 reports...
         var SpecReporter = require('jasmine-spec-reporter');
-        jasmine.getEnv().addReporter(new SpecReporter({displayStacktrace: 'spec'}));
+        jasmine.getEnv().addReporter(new SpecReporter({ displayStacktrace: 'spec' }));
     },
 
     capabilities: {
-		browserName: 'chrome'
-	},
+        browserName: 'chrome'
+    },
 
     jasmineNodeOpts: {
         showColors: true,
         displayStacktrace: true,
         displaySpecDuration: true,
         // overrides jasmine's print method to report dot syntax for custom reports
-        print: function () {},
+        print: function () { },
         defaultTimeoutInterval: 50000
     }
 };
